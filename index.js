d971d43b2d0546d8fb3ea27a5cac9f96c0d12347
Joseph Lee <jslee5413@gmail.com>
2023-11-24 11:52:36 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+}
+
+function countDown(number) {
+    let i = number
+    while (i>=0) {
+        console.log(i--)
+    }
+}
